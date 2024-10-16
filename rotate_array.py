a89a2a872123845fb4f5f788c595254299854864
dmitry-kis <dmitry-kis@novami.com>
2024-10-16 10:41:54 -0700

    Update rotate-array problem solution

--- rotate_array.py
+++ rotate_array.py
@@ -3,9 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for indk in range(k):
-            temp = nums[-1]
-            for ind in range(len(nums)):
+        N = len(nums)
+        if k % N == 0:
+            return
+        if 2 * k == N:
+            for ind in range(N // 2):
+                temp = nums[(k + ind) % N]
+                nums[(k + ind) % N] = nums[ind]
+                nums[ind] = temp
+        else:
+            temp = nums[- k % N]
+            ind = 0
+            print(ind, temp)
+            for _ in range(N):
                 temp1 = nums[ind]
                 nums[ind] = temp
                 temp = temp1
+                ind = (k + ind) % N
+                print(ind, temp, nums)
