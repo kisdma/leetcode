bca7bdf1c214a783dd6587fe5b0f2dea18324ad5
dmitry-kis <dmitry-kis@novami.com>
2024-10-16 19:09:06 -0700

    Update jump-game problem solution

--- jump_game.py
+++ jump_game.py
@@ -2,10 +2,26 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         if len(nums) == 1:
             return True
-        if nums[0] > 0:
-            if len(nums)-1 <= nums[0]:
-                return True
-            for ind2 in range(1, nums[0]+1):
-                if self.canJump(nums[ind2:]):
-                    return True
-        return False
+        zero_ind = len(nums) - 1
+        zero_flag = False
+        cant_jump_over = False
+        zero_count = 0
+        nonzero_flag = True
+        for ind1 in range(len(nums)-2, -1, -1):
+            if nums[ind1] == 0:
+                if nonzero_flag:
+                    zero_count = 0
+                    nonzero_flag = False
+                else:
+                    zero_count += 1
+                if cant_jump_over:
+                    return False
+                cant_jump_over = True
+                zero_ind = ind1
+                zero_flag = True
+            elif zero_flag:
+                nonzero_flag = True
+                if nums[ind1] > zero_ind - ind1 + zero_count:
+                    cant_jump_over = False
+            print(zero_ind, zero_flag, cant_jump_over, zero_count)
+        return not cant_jump_over
