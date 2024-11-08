633ba1f6284835d00aed33311b2ec154d462ace4
dmitry-kis <dmitry-kis@novami.com>
2024-11-08 10:48:16 -0800

    Updating triangle.py with more efficient solution

--- triangle.py
+++ triangle.py
@@ -3,11 +3,16 @@ class Solution:
         N = len(triangle)
         if N == 1:
             return triangle[0][0]
-        def onelayer(level, ind):
-            if level < N - 2:
-                return triangle[level][ind] + \
-                    min(onelayer(level + 1, ind), onelayer(level + 1, ind + 1))
-            else:
-                return triangle[level][ind] + \
-                    min(triangle[level + 1][ind], triangle[level + 1][ind + 1])
-        return onelayer(0, 0)
+        if N == 2:
+            return triangle[0][0] + min(triangle[1])
+        up_to_level = [0] * N
+        up_to_level[0] = triangle[0][0]
+        for ind in range(1, N):
+            temp = up_to_level[0]
+            for ind1 in range(1, ind):
+                temp1 = up_to_level[ind1]
+                up_to_level[ind1] = min(temp, up_to_level[ind1]) + triangle[ind][ind1]
+                temp = temp1
+            up_to_level[0] = up_to_level[0] + triangle[ind][0]
+            up_to_level[ind] = temp + triangle[ind][-1]
+        return min(up_to_level)
