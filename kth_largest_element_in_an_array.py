0ec1b2b45d280093ae6a890da01602c845fe2c74
dmitry-kis <dmitry-kis@novami.com>
2024-11-10 22:52:06 -0800

    Create preliminary inefficient but functional version of kth_largest problem

--- kth_largest_element_in_an_array.py
+++ kth_largest_element_in_an_array.py
@@ -22,13 +22,39 @@ class Solution:
                     h = h1
                 else:
                     not_sorted_flag = False
+
         ind = len(nums)
         while ind > 0:
             print(nums[ind - 1])
             ind = ind >> 1
         print('------')
+
+        start_ind = len(nums)
+        end_ind = len(nums) >> 1
+        for ind in range(start_ind, end_ind, -1):
+            sort_branch(nums, ind)
+
+        not_sorted_flag = True
+        while not_sorted_flag:
+            start_ind = len(nums) - 1
+            # end_ind = ((len(nums) + 1) >> 1) - 2
+            end_ind = 0
+            print(nums[end_ind + 1:start_ind+1])
+            for ind in range(start_ind, end_ind, -1):
+                if nums[ind - 1] < nums[ind]:
+                    temp = nums[ind - 1]
+                    nums[ind - 1] = nums[ind]
+                    nums[ind] = temp
+                    sort_branch(nums, ind)
+                    sort_branch(nums, ind + 1)
+                    break
+            else:
+                not_sorted_flag = False
+
         ind = len(nums)
-        sort_branch(nums, ind)
         while ind > 0:
             print(nums[ind - 1])
             ind = ind >> 1
+
+        print(nums)
+        return nums[k-1]
