be75a169bdf1b10a0ba2032607aafe741c6dca1a
dmitry-kis <dmitry-kis@novami.com>
2024-11-13 15:35:13 -0800

    Update solution for kth_largest problem

--- kth_largest_element_in_an_array.py
+++ kth_largest_element_in_an_array.py
@@ -5,53 +5,54 @@ def findKthLargest(nums, k):
         return max(nums)
     if k == len(nums):
         return min(nums)
-    def return_sorted_idx():
-        pass
-    def sort_branch(nums, ind):
-        if nums[(ind >> 1) - 1] >= nums[ind - 1]:
+
+    def sort_branch(nums, ind, shift=0):
+        if nums[(ind >> 1) - 1 + shift] >= nums[ind - 1 + shift]:
             return
-        if nums[0] < nums[ind - 1]:
-            move_branch(nums, ind, ind.bit_length() - 1)
+        if nums[0 + shift] < nums[ind - 1 + shift]:
+            move_branch(nums, ind, ind.bit_length() - 1, shift)
             return
         intervals = (ind.bit_length() - 2) >> 1
         pos = intervals
         while intervals > 0:
-            if nums[(ind >> (pos + 1)) - 1] >= nums[ind - 1]:
-                if intervals == 3:
-                    intervals = 2
-                elif intervals == 2:
-                    intervals = 1
-                else:
-                    intervals = (intervals >> 1) + (intervals & 1)
+            if nums[(ind >> (pos + 1)) - 1 + shift] >= nums[ind - 1 + shift]:
+                intervals = (intervals >> 1) + (intervals & 1)
                 pos -= intervals
             else:
                 if intervals == 3:
                     intervals = 2
-                elif intervals == 2:
-                    intervals = 1
                 else:
-                    intervals = (intervals >> 1)# + (intervals & 1)
+                    intervals = (intervals >> 1)
                 pos += intervals
             if pos < 0:
-                move_branch(nums, ind, 1)
+                move_branch(nums, ind, 1, shift)
                 return
-        if nums[(ind >> (pos + 1)) - 1] >= nums[ind - 1]:
-            move_branch(nums, ind, pos)
+        if nums[(ind >> (pos + 1)) - 1 + shift] >= nums[ind - 1 + shift]:
+            move_branch(nums, ind, pos, shift)
             return
         else:
-            move_branch(nums, ind, pos + 1)
+            move_branch(nums, ind, pos + 1, shift)
             return
-    def move_branch(nums, ind, n):
+        
+    def move_branch(nums, ind, n, shift=0):
         # print(nums, ind, n)
-        temp = nums[ind - 1]
+        temp = nums[ind - 1 + shift]
         for i in range(n):
             ind1 = ind >> 1
-            nums[ind - 1] = nums[ind1 - 1]
+            nums[ind - 1 + shift] = nums[ind1 - 1 + shift]
             ind = ind1
-        nums[ind - 1] = temp
+        nums[ind - 1 + shift] = temp
         return
 
-    for ind in range(1, len(nums)):
+    ind = 2
+    while ind < len(nums):
+        if nums[ind - 1] < nums[ind]:
+            temp = nums[ind - 1]
+            nums[ind - 1] = nums[ind]
+            nums[ind] = temp
+        sort_branch(nums, ind)
+        ind += 2
+    if not(len(nums) & 1):
         sort_branch(nums, ind)
 
     ind = len(nums)
